c0d04f53cb2c056e59ff98859059dff526277c5b
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-10-22 09:34:30 +0500

    Completed

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const hamb = document.querySelector('.hamburger')
 const navBar = document.querySelector('.nav-bar')
 hamb.addEventListener('click', () => {
     navBar.classList.toggle('active')
+    hamb.classList.toggle('rotate90')
 })
